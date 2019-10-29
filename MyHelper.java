3c4c44bc560edea5f01d24f249d143bb6267af5e
SojinPark1 <57087514+SojinPark1@users.noreply.github.com>
2019-10-29 02:42:14 -0700

    Create MyHelper.java

---
+++ MyHelper.java
@@ -0,0 +1,45 @@
+package com.example.assignment2final;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MyHelper extends SQLiteOpenHelper { //Helperclass
+    private Context context;
+    private static final String CREATE_TABLE =
+            "CREATE TABLE "+
+                    Constants.TABLE_NAME + " (" +
+                    Constants.UID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    Constants.WL + " TEXT, " +
+                    Constants.SHOTS + " TEXT, " +
+                    Constants.SAVES + " TEXT);" ;
+
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + Constants.TABLE_NAME;
+
+    public MyHelper(Context context){
+        super (context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(CREATE_TABLE);
+        } catch (SQLException e) {
+            Toast.makeText(context, "exception onCreate() db", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { // CAN MODIFY, DELETE THE TABLE, ADD/DELETE COLUMNS AND DROP TABLES
+        try {
+            db.execSQL(DROP_TABLE);
+            onCreate(db);
+            Toast.makeText(context, "onUpgrade called", Toast.LENGTH_LONG).show();
+        } catch (SQLException e) {
+            Toast.makeText(context, "exception onUpgrade() db", Toast.LENGTH_LONG).show();
+        }
+    }
+}
